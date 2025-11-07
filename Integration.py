5bcf6bdf38e3c482fdaded56de07f7480d81a3ad
719098 <35185657abc@gmail.com>
2025-11-07 20:36:11 +0800

    Initial commit

---
+++ Integration.py
@@ -0,0 +1,73 @@
+# initial environment
+# import modules
+import tkinter as tk
+import sqlite3
+
+# basic GUI 
+root = tk.Tk()
+root.title('INTEGRATION')
+root.geometry('300x400')  # 調整視窗大小
+
+# student ID label and entry
+label_id = tk.Label(root, text='Student ID')
+label_id.pack(pady=(15,5))
+entry_id = tk.Entry(root, width=25)
+entry_id.pack()
+
+# student name label and entry
+label_name = tk.Label(root, text='Student Name')
+label_name.pack(pady=(10,5))
+entry_name = tk.Entry(root, width=25)
+entry_name.pack()
+
+# setting print_student function
+def print_student():
+    student_id = entry_id.get()
+    student_name = entry_name.get()
+    print('Student ID: {}'.format(student_id))
+    print('Student Name: {}'.format(student_name))
+    print('-'*30)
+
+# Print button
+button_print = tk.Button(root, text='Print', command=print_student)
+button_print.pack(pady=15)
+
+# connect to database
+conn = sqlite3.connect('Student.db')
+cursor = conn.cursor()
+
+# create_student function
+def create_student():
+    student_id = entry_id.get()
+    student_name = entry_name.get().lower()
+    cursor.execute('INSERT INTO DB_student (db_student_id, db_student_name) VALUES(?,?)',
+                   (student_id, student_name))
+    conn.commit()
+    print('Student ID: {}'.format(student_id))
+    print('Student Name: {}'.format(student_name))
+    print('-'*30)
+
+button_create = tk.Button(root, text='Create', command=create_student)
+button_create.pack(pady=20)
+
+# overview_student function
+def overview_student():
+    cursor.execute('SELECT * FROM DB_student')
+    overview = cursor.fetchall()
+    print(overview)
+
+button_overview = tk.Button(root, text='Overview', command=overview_student)
+button_overview.pack(pady=25)
+
+# delete_student function
+def delete_student():
+    student_id = entry_id.get()
+    cursor.execute('DELETE FROM DB_student WHERE db_student_id = ?', (student_id,))
+    conn.commit()
+    print(f'Deleted Student ID: {student_id}')
+    print('-'*30)
+
+button_delete = tk.Button(root, text='Delete', command=delete_student)
+button_delete.pack(pady=20)
+
+root.mainloop()
